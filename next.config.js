9d954879b49e9156d66fbef8dc556a39639fb941
Nikita Madeev <warhammer-ss@yandex.ru>
2022-07-04 16:27:12 +0300

    fix: :rocket: Fix hosting Next.js on GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const isProd = process.env.NODE_ENV === 'production';
+const repositoryName = 'authentication-form';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    // Fix hosting Next.js on GitHub Pages
+    // https://github.com/vercel/next.js/issues/3335
+    basePath: isProd ? `/${repositoryName}` : '',
     experimental: {
         images: {
             unoptimized: true,
