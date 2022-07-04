f090359942d38a057ab3779b989e1be33f731d54
Nikita Madeev <warhammer-ss@yandex.ru>
2022-07-04 18:35:19 +0300

    feat: :construction: Start add form

--- src/components/home-page/HomePage.tsx
+++ src/components/home-page/HomePage.tsx
@@ -1,5 +1,12 @@
+import { Background } from '../background/Background';
 import { Form } from '../form/Form';
+import styles from './HomePage.module.css';
 
 export const HomePage: React.FC = () => {
-    return <Form />;
+    return (
+        <main className={styles.container}>
+            <Background className={styles.background} />
+            <Form />
+        </main>
+    );
 };
