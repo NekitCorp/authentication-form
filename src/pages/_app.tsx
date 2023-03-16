4a6112656d7c0c9d22a62314596e00281272ccb2
Nikita Madeev <warhammer-ss@yandex.ru>
2023-03-16 15:34:39 +0300

    refactor: :wastebasket: Clean up next.js start project.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,21 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import localFont from 'next/font/local';
+import Head from 'next/head';
 
 // Font files can be colocated inside of `pages`
 const gilroyFont = localFont({ src: './Gilroy-Light.otf' });
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <main className={gilroyFont.className}>
-            <Component {...pageProps} />
-        </main>
+        <>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="favicon.ico" />
+            </Head>
+            <main className={gilroyFont.className}>
+                <Component {...pageProps} />
+            </main>
+        </>
     );
 }
