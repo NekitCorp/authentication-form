941cbb0a3c18be119fa1d788cd650daebe0c6eb9
Nikita Madeev <warhammer-ss@yandex.ru>
2023-03-16 15:19:13 +0300

    feat: :lipstick: Add Gilroy font.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import localFont from 'next/font/local';
+
+// Font files can be colocated inside of `pages`
+const gilroyFont = localFont({ src: './Gilroy-Light.otf' });
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <main className={gilroyFont.className}>
+            <Component {...pageProps} />
+        </main>
+    );
 }
