85247d97888e62a28b1d634d40b9ec3926d26f35
NekitCorp <NekitCorp@users.noreply.github.com>
2023-03-16 19:10:39 +0000

    deploy: 3841015e1281aef8925fa36fc1bb08917bfd2beb

--- _next/static/RaNFbR77wfL0dj-PqLwrq/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/81af8bfe0a4b5b44.css","static/chunks/pages/index-4f18a237ebafb5e2.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
