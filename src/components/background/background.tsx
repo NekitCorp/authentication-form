aeaf99e99ea28f0658d7dc8953ee417bfb7b21a0
Nikita Madeev <warhammer-ss@yandex.ru>
2023-03-17 15:50:15 +0300

    feat: :construction: Add form styles.

--- src/components/background/background.tsx
+++ src/components/background/background.tsx
@@ -9,11 +9,12 @@ import { elements } from './data';
 export const Background: React.FC = () => {
     const mousePosition = useMousePosition();
     const prefersReducedMotion = usePrefersReducedMotion();
-    const { width, height } = useWindowSize();
+    const windowSize = useWindowSize();
     const isTouchDevice = useMedia('(hover: none)', true);
 
-    const x = mousePosition.x / width;
-    const y = mousePosition.y / height;
+    const motionEnable = !prefersReducedMotion && !isTouchDevice;
+    const x = mousePosition.x / windowSize.width;
+    const y = mousePosition.y / windowSize.height;
 
     return (
         <div className={styles.container}>
@@ -22,12 +23,11 @@ export const Background: React.FC = () => {
                     key={i}
                     className={clsx(styles.element, element.className)}
                     style={{
-                        transform:
-                            prefersReducedMotion || isTouchDevice
-                                ? undefined
-                                : `translate(${Math.ceil(x * element.motionSpeed)}px, ${Math.ceil(
-                                      y * element.motionSpeed,
-                                  )}px)`,
+                        transform: motionEnable
+                            ? `translate(${Math.ceil(x * element.motionSpeed)}px, ${Math.ceil(
+                                  y * element.motionSpeed,
+                              )}px)`
+                            : undefined,
                     }}
                 >
                     <Image src={element.src} alt="" fill />
