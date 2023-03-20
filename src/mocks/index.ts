22e9d04705f00b2d48d367b6490e4d2d7e048cd3
Nikita Madeev <warhammer-ss@yandex.ru>
2023-03-20 10:37:18 +0300

    fix: :bug: Fix MSW baseUrl.

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -8,6 +8,7 @@ async function initMocks() {
         const { worker } = await import('./browser');
         worker.start({
             onUnhandledRequest: 'bypass',
+            // https://github.com/mswjs/msw/issues/690#issuecomment-849552403
             serviceWorker: {
                 // Specify the worker script URL relative to the _root_.
                 url: `${PREFIX}/mockServiceWorker.js`,
@@ -15,7 +16,7 @@ async function initMocks() {
                     // Override the scope to the root ("/").
                     // By default, the worker is scoped to its location on your server,
                     // which in this case would be "/prefix".
-                    scope: '/',
+                    scope: PREFIX,
                 },
             },
         });
