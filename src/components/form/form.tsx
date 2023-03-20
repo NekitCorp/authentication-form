7b66cefb133d4111034b75fff2cb3a897389c55a
Nikita Madeev <warhammer-ss@yandex.ru>
2023-03-20 10:52:42 +0300

    fix: :bug: Fix MSW baseUrl.

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -26,7 +26,7 @@ export const Form: React.FC = () => {
         };
 
         try {
-            const response = await fetch('/api/form', {
+            const response = await fetch('api/form', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(data),
